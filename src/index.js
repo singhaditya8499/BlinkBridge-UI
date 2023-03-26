1c03e00eebea4b568b23b01676212112847f3156
Aditya Singh <singh.aditya8499@gmail.com>
2023-03-26 07:51:15 -0400

    Add code for BlinkBridge UI

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; //convert JSX to readable form
+import ReactDOM from 'react-dom'; //for rendering to page
+// import App from './Components/App';
+import App from './Components/App';
+
+// ReactDOM.render(<App />,document.getElementById('root'));
+
+ReactDOM.render(<App/>,document.getElementById("root"));
